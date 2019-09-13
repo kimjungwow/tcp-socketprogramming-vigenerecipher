cbce5557b18e8b77fbe57c8be376ab7f743c947e
kimjungwow <jw950310@kaist.ac.kr>
2019-09-14 00:01:30 +0900

    Client done except last one letter

--- client.c
+++ client.c
@@ -18,10 +18,12 @@
 #define KEYWORD "cake"
 
 #define MAXDATASIZE 1024*1024 // max number of bytes we can get at once
+#define MAXPACKETSIZE 1024*1024*10-16
 
 struct header {
   unsigned short op;
   unsigned short checksum;
+  unsigned short checksum_temp;
   char keyword[4];
   unsigned long long length;
   uint32_t nworder_length;
@@ -64,11 +66,11 @@ int main(int argc, char *argv[]) {
   calculate_checksum(&myheader->checksum,
       (unsigned short)(myheader->keyword[2])*(unsigned short)(256) + (unsigned short)(myheader->keyword[3]));
 
-
+  myheader->checksum_temp=myheader->checksum;
 
 
   int socket_fd, numbytes;
-  char* buf = (char *)malloc(MAXDATASIZE * sizeof(char));
+  
   struct addrinfo hints, *servinfo, *p;
   int rv;
   char s[INET6_ADDRSTRLEN];
@@ -104,83 +106,111 @@ int main(int argc, char *argv[]) {
   }
 
   freeaddrinfo(servinfo); // all done with this structure
-  unsigned char buffer_stdin[MAXDATASIZE];
-  unsigned char buffer_stdintw[MAXDATASIZE];
 
-  unsigned char *buffertemp = (unsigned char *)(MAXDATASIZE * sizeof(unsigned char));
-  unsigned char *buffertempano = (unsigned char *)(MAXDATASIZE * sizeof(unsigned char));
-  buffertemp=buffer_stdintw;
-  buffertempano=buffer_stdin;
+
+  
+  unsigned char *buffertempano = (unsigned char *)malloc(MAXDATASIZE * sizeof(unsigned char));
 
   int bytesread = MAXDATASIZE;
-  while(fgets(buffertempano, sizeof(buffer_stdin), stdin)!=NULL) {
-      strcat(buffertemp,buffertempano);
+  while(fgets(buffertempano, sizeof(buffertempano), stdin)!=NULL) {
+        // printf("LOL\n");
+        
+        myheader->checksum=myheader->checksum_temp;
+        unsigned char *buffertemp = (unsigned char *)malloc(MAXPACKETSIZE * sizeof(unsigned char));
+        strcat(buffertemp,buffertempano);
+        while(strlen(buffertemp)+MAXDATASIZE<MAXPACKETSIZE && fgets(buffertempano, sizeof(buffertempano), stdin)!=NULL) {
+            strcat(buffertemp,buffertempano);
+            // printf("strlen %zu + %zu | MAXPACKETSIZE %zu\n",strlen(buffertemp),MAXDATASIZE,MAXPACKETSIZE);
+            
+        }
+        
+        for (int b = 0; b < strlen(buffertemp); b++) {
+            buffertemp[b] = tolower(buffertemp[b]);
+        }
+        unsigned char *buffer = (unsigned char *)malloc(sizeof(char) * (strlen(buffertemp) + 15));
+
+        myheader->length += strlen(buffertemp) - 1;
+        myheader->nworder_length = htonl((uint32_t)(myheader->length));
+        
+
+        for (int c = 0 ; c < (strlen(buffertemp)-1)/2 ; c++ ) {
+            calculate_checksum(&myheader->checksum,
+            (unsigned short)(buffertemp[2*c])*(unsigned short)(256) + (unsigned short)(buffertemp[2*c+1]));
+        }
+        
+        if (strlen(buffertemp)%2==0)
+            calculate_checksum(&myheader->checksum, (unsigned short)(256)*(unsigned short)(buffertemp[strlen(buffertemp)-2]));
+            
+        calculate_checksum(&myheader->checksum,(unsigned short)(myheader->length%65536));
+        myheader->checksum = ~(myheader->checksum);
+        myheader->checksum = htons(myheader->checksum);
+        myheader->length = htobe64(myheader->length);
+
+        memset(buffer, 0, 1);
+        memcpy(buffer + 1, &myheader->op, sizeof(char));
+        memcpy(buffer + 2, &myheader->checksum, sizeof(myheader->checksum));
+        memcpy(buffer + 4, &myheader->keyword, sizeof(char) * 4); 
+        memcpy(buffer + 8, &myheader->length, sizeof(unsigned long long));
+        memcpy(buffer + 16, buffertemp, strlen(buffertemp));
+
+        
+        unsigned char *cursor = buffer;
+        /*
+        printf("buffer : %s\n", buffertemp);
+        for(int z=0;z<strlen(buffertemp)+15;z++) {
+            printf("%02x ",*cursor);
+            if(z%4==3)
+                printf("| ");
+            ++cursor;
+        }
+        printf("\n");
+
+        */
+        //   send(socket_fd, buffer, strlen(buffertemp) + 15, 0);
+        int readbytes = be64toh(myheader->length), tempreadbytes=readbytes;
+        send(socket_fd, buffer, readbytes, 0);
+        // printf("client: send %d bytes\n", readbytes);
+        numbytes=10;
+        // while( numbytes>0) {
+        while(readbytes>0) {
+            char* buf = (char *)malloc(MAXPACKETSIZE * sizeof(char));
+            
+            // printf("HERE\n");
+            if ((numbytes = recv(socket_fd, buf, MAXPACKETSIZE - 1, 0)) == -1) {
+                perror("recv");
+                exit(1);
+            }
+            // printf("Read %d numbytes\n",numbytes);
+            
+            buf[numbytes] = '\0';
+            int startindex=0;
+            if(readbytes==tempreadbytes)
+                startindex=16;
+            // printf("client: received '%s' with %d bytes\n", buf,numbytes);
+            for (int y=startindex; y<numbytes;y++) {
+                printf("%c",buf[y]);
+            }
+            // printf("\n");
+            free(buf);
+            readbytes-=(numbytes);
+            // printf("LEFT %d bytes\n", readbytes);
+            // printf("Read %d numbytes\n",numbytes);
+
+        }
+        
+        // printf("\n");
+
+        free(buffertemp);
+        free(buffer);
+
   }
-
-//   fgets(buffertemp, sizeof(buffer_stdin), stdin);
-
-  for (int b = 0; b < strlen(buffertemp); b++) {
-      buffertemp[b] = tolower(buffertemp[b]);
-  }
-  unsigned char *buffer = (unsigned char *)malloc(sizeof(char) * (strlen(buffertemp) + 15));
-
-  myheader->length += strlen(buffertemp) - 1;
-  myheader->nworder_length = htonl((uint32_t)(myheader->length));
   
-
-  for (int c = 0 ; c < (strlen(buffertemp)-1)/2 ; c++ ) {
-      calculate_checksum(&myheader->checksum,
-      (unsigned short)(buffertemp[2*c])*(unsigned short)(256) + (unsigned short)(buffertemp[2*c+1]));
-  }
   
-  if (strlen(buffertemp)%2==0)
-      calculate_checksum(&myheader->checksum, (unsigned short)(256)*(unsigned short)(buffertemp[strlen(buffertemp)-2]));
-      
-  calculate_checksum(&myheader->checksum,(unsigned short)(myheader->length%65536));
-  myheader->checksum = ~(myheader->checksum);
-  myheader->checksum = htons(myheader->checksum);
-  myheader->length = htobe64(myheader->length);
-
-  memset(buffer, 0, 1);
-  memcpy(buffer + 1, &myheader->op, sizeof(char));
-  memcpy(buffer + 2, &myheader->checksum, sizeof(myheader->checksum));
-  memcpy(buffer + 4, &myheader->keyword, sizeof(char) * 4); 
-  memcpy(buffer + 8, &myheader->length, sizeof(unsigned long long));
-  memcpy(buffer + 16, buffertemp, strlen(buffertemp));
-
-  
-  unsigned char *cursor = buffer;
-/*
-  printf("buffer : %s\n", buffertemp);
-  for(int z=0;z<strlen(buffertemp)+15;z++) {
-      printf("%02x ",*cursor);
-      if(z%4==3)
-          printf("| ");
-      ++cursor;
-  }
-  printf("\n");
-
-  */
-//   send(socket_fd, buffer, strlen(buffertemp) + 15, 0);
-  send(socket_fd, buffer, be64toh(myheader->length), 0);
-
-  if ((numbytes = recv(socket_fd, buf, MAXDATASIZE - 1, 0)) == -1) {
-      perror("recv");
-      exit(1);
-  }
-
-  buf[numbytes] = '\0';
-
-//   printf("client: received '%s' with %d bytes\n", buf,numbytes);
-  for (int y=16; y<numbytes;y++) {
-      printf("%c",buf[y]);
-  }
-  printf("\n");
   close(socket_fd);
 
   free(host);
   free(port);
   free(myheader);
-  free(buffer);
+  
   return 0;
 }
